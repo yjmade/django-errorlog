aa4d4415b4e77d8b68d92ad5f4f62606e956bc48
Jay Young <bitbucket@yjmade.net>
2016-12-04 18:52:09 +0800

    [ADD] admin for error

---
+++ errorlog/admin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+# Register your models here.
+from .models import Error
+
+
+@admin.register(Error)
+class ErrorAdmin(admin.ModelAdmin):
+    list_filter = [
+        "fixed",
+    ]
+    list_display = [
+        "error_message",
+        "occur_time"
+    ]
